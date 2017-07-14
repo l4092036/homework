aad2d3ed34b4fd12060f5c2b43ab07a8849f6d14
Ruby <Ruby@1704002NB01>
2017-07-14 17:34:43 +0800

    Version1

---
+++ HomeWorkMVC/HomeWorkMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeWorkMVC.Startup))]
+namespace HomeWorkMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
