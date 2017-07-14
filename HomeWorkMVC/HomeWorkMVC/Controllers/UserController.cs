bb9e74f2d7d329ddad76240586adebb7861b3a0d
Ruby <Ruby@1704002NB01>
2017-07-14 18:44:58 +0800

    hw end

---
+++ HomeWorkMVC/HomeWorkMVC/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeWorkMVC.Models;
+using HomeWorkMVC.Models.ViewModels;
+
+namespace HomeWorkMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LogonViewModel pageData)
+        {
+            if (pageData.Account == "ruby.lin@iisigroup.com" && pageData.Password == "tree")
+            {
+                pageData.Message = $"您使用{pageData.Account}登入成功。";
+
+            }
+            return View(pageData);
+        }
+    }
+}
---
+++ HomeWorkMVC/HomeWorkMVC/Models/ViewModels/LogonViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using HomeWorkMVC.ValidateAttribute;
+
+namespace HomeWorkMVC.Models.ViewModels
+{
+    public class LogonViewModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"\w.+\@\w.+")]
+        [NoIs("skilltree,demo,twMVC", ErrorMessage = "請使用別的名稱")]
+        public string Account { set; get; }
+        [StringLength(20,MinimumLength =4)]
+        [Display(Name ="密碼")]
+        public string Password { set; get; }
+        
+        public string Message { set; get; }
+    }
+}
